092c49ddcdc9f5a30413e6973d5f4086d0802878
gonzalezad1 <gonzalez_ad@hotmail.fr>
2019-01-09 10:39:29 +0100

    refacto: add every fils inside one

--- db/migrations/20190102162625_renameUsersTis.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.renameTable("users_tis", "user_tis");
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.renameTable("user_tis", "users_tis");
-};
